74a381fee6eca563215e784a17cb955cff707e10
MuthuprabhaT <prabhathangam507@hotmail.com>
2023-10-24 19:18:37 +0530

    first commit

--- Day4-Task/func.js
+++ Day4-Task/func.js
@@ -32,6 +32,36 @@ b();
 
 //2. Convert all the strings to tittle caps in a string array
 //anonymous:
+let str="converting string to tittle case";
+let b=function () {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    } 
+   return str.join(' ');
+  }
+  console.log(b());
+
+//IIFE:
+(function (str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    } 
+   console.log(str.join(' '));
+  })
+  ("converting string to tittle case");
+
+  //arrow:
+  let str="converting string to tittle case";
+  let b=()=> {
+  str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    } 
+   return str.join(' ');
+  }
+  console.log(b());
 
 //3. Sum of all numbers in an array
 //anonymous:
@@ -55,4 +85,42 @@ let b = a.reduce((x,y) => {
     return x+y;
 },)
 console.log(b);
+
+//4. Return all prime numbers in an array.
+//anonymous:
+let numArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,977,975,6997]
+let b=function(){
+    numArray = numArray.filter((number) => {
+    for (var i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+}
+    return true;
+});
+    console.log(numArray);
+}
+b();
+
+//IIFE:
+(function(numArray){
+    numArray = numArray.filter((number) => {
+    for (var i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) return false;
+    }
+        return true;
+    });
+       console.log(numArray);
+})([1,2,3,4,5,6,7,8,9,10,11,12,13,977,975,6997])
+
+//arrow:
+let numArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,977,975,6997]
+let b=()=>{
+    numArray = numArray.filter((number) => {
+    for (var i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+}
+    return true;
+});
+    console.log(numArray);
+}
+b();
 
